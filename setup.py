99b12fe88f57662f42cea00774d13c048724ce20
Casey Beach <beachc@gmail.com>
2017-10-22 16:16:08 -0700

    preparation for pypi publication

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(name='nes_le',
           "numpy==1.13.3",
           "opencv-python==3.3.010",
       ],
-      url='https://github.com/cbeach/NES-LE',
+      url='https://github.com/cbeach/nes_le',
       packages=['nes_le', 'nes_le.game_state_interfaces'],
+      download_url='https://codeload.github.com/cbeach/nes_le/tar.gz/0.0.1',
       )
